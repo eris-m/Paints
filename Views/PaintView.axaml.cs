7625c7e1e96edbabf076e3b3f3ef265edfe317e2
LillyButSilly <lillypannowitz@gmail.com>
2025-05-23 21:28:08 +1000

    Moved paint view to allow automatic finding.

--- Views/PaintView.axaml.cs
+++ Views/PaintView.axaml.cs
@@ -2,11 +2,11 @@ using Avalonia;
 using Avalonia.Controls;
 using Avalonia.Markup.Xaml;
 
-namespace Paints.Controls;
+namespace Paints.Views;
 
-public partial class PaintControl : UserControl
+public partial class PaintView : UserControl
 {
-    public PaintControl()
+    public PaintView()
     {
         InitializeComponent();
     }
