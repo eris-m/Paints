b6f7e1d92603de3639db5fd81f57f2e42c3e7073
Eris M <erismathison@gmail.com>
2025-05-28 21:07:32 +1000

    Added stock tracking modelling.

--- Models/Paint.cs
+++ Models/Paint.cs
@@ -1,8 +1,9 @@
+using System;
 using Avalonia.Media;
 
 namespace Paints.Models;
 
-public class Paint(string name, string brand, Color colour)
+public class Paint(string name, string brand, Color colour) : IEquatable<Paint>
 {
     public Paint() : this( "" , "" , Colors.Black)
     {}
@@ -10,4 +11,12 @@ public class Paint(string name, string brand, Color colour)
     public string Name { get; set; } = name;
     public string Brand { get; set; } = brand;
     public Color Colour { get; set; } = colour;
+
+    public bool Equals(Paint? other) => other != null && Name == other.Name;
+}
+
+public class PaintStock(Paint paint, uint stock = 0)
+{
+    public Paint Paint { get; set; } = paint;
+    public uint Stock { get; set; } = stock;
 }
--- ViewModels/InfoPageViewModel.cs
+++ ViewModels/InfoPageViewModel.cs
@@ -1,13 +1,29 @@
+using System.Collections.Generic;
 using System.Windows.Input;
 using CommunityToolkit.Mvvm.ComponentModel;
 using Paints.Models;
 
 namespace Paints.ViewModels;
 
-public partial class InfoPageViewModel : ViewModelBase
+public partial class InfoPageViewModel(PaintViewModel? paint, Dictionary<string, PaintStock> paintStocks) : ViewModelBase
 {
     [ObservableProperty]
-    private PaintViewModel _paint;
-    
+    private PaintViewModel? _paint = paint;
+
+    private Dictionary<string, PaintStock> _paintStocks = paintStocks;
+
+    public uint InStock
+    {
+        get
+        {
+            if (Paint == null)
+                return 0;
+            
+            if (!paintStocks.TryGetValue(Paint.Name, out var value))
+                return 0;
+            return value.Stock;
+        }
+    }
+
     public ICommand? ToListCommand { get; set; }
 }
