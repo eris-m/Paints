9de5e5b1851a8208cebee5fa53daa3ba829d9b89
Eris M <erismathison@gmail.com>
2025-06-01 10:10:01 +1000

    Add paint adding. (#9)
    * Added dialogue service.
    
    * Added dialogue.
    
    * Fixed styling.

--- Services/IDialogueService.cs
+++ Services/IDialogueService.cs
@@ -1,5 +1,7 @@
 using System.Threading.Tasks;
+using Avalonia.Controls;
 using Avalonia.Controls.ApplicationLifetimes;
+using Paints.Models;
 using Paints.Views;
 
 namespace Paints.Services;
@@ -7,13 +9,35 @@ namespace Paints.Services;
 public interface IDialogueService
 {
     public Task<uint?> ChangeStock();
-    
+    public Task<Paint?> CreatePaint();
 }
 
 public class DialogueService : IDialogueService
 {
     public async Task<uint?> ChangeStock()
     {
+        var window = new ChangeStockDialogue();
+
+        var newStock = await window.ShowDialog<uint?>(MainWindow()!);
+        return newStock;
+    }
+
+    public async Task<Paint?> CreatePaint()
+    {
+        var window = new AddPaintDialogue();
+
+        var newPaint = await window.ShowDialog<Paint?>(MainWindow()!);
+        return newPaint;
+    }
+
+    private Window? MainWindow()
+    {
+        var app = App.Current;
+
+        if (app?.ApplicationLifetime is not IClassicDesktopStyleApplicationLifetime desktop)
+            return null;
+
+        return desktop.MainWindow;
         
         var window = new ChangeStockDialogue();
 
